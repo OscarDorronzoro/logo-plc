b66b664f478430a5de60718aa4df1a579082d4f1
Oscar Dorronzoro <oscar.dorronzoro@gmail.com>
2025-01-29 00:16:24 -0300

    signals defined, untested on logo!

--- logo.py
+++ logo.py
@@ -1,70 +1,167 @@
 import tkinter as tk
 import snap7
 from snap7.util import *
-from snap7.type import *
+from snap7.types import *
 import threading
 import time
 
 # Instalar dependencias
-# pip install tkinder python-snap7
+# pip install python-snap7
+# sudo add-apt-repository ppa:gijzelaar/snap7
+# sudo apt update
+# sudo apt install libsnap7-1 libsnap7-dev
+# sudo apt-get install python3-tk
 
-# Contantes
-signals_mem = {
-            'hab_entrar_A': 0,
-            'hab_entrar_B': 1,
-            'lista_pesada': 2,
-            'camion_a_balanza': 3,
-            'balanza_en_cero': 4,
-            'listo': 5,
-            'listo_pesar': 6
+# Levantar servidor falso s7
+# python3 -m snap7.server --port 102
+
+# Configurar red
+# sudo ip address flush dev enp9s0
+# sudo ip route flush dev enp9s0
+# sudo ip address add 192.168.0.5/24 brd + dev enp9s0
+# sudo ip route add 192.168.0.1 dev enp9s0
+# sudo ip route add default via 192.168.0.1 dev enp9s0
+# ip a s dev enp9s0
+
+
+# Constantes
+signals_M = {
+            # Escritura de pc a logo
+            'hab_general': 'V948.0', #M1
+            'reajuste': 'V948.1', #M2 (reservada)
+            'hab_entrar_A': 'V948.2', #M3
+            'hab_entrar_B': 'V948.3', #M4
+            'camion_en_balanza': 'V948.4', #M5
+            'fin_pesada': 'V948.5', #M6
+            'balanza_en_cero': 'V948.6', #M7
+            
+            # Uso interno
+            'uso_interno_1': 'V948.7', #M8
+            
+            # Lectura de logo a pc
+            'listo': 'V949.0', #M9
+            'ingreso_A': 'V949.1', #M10
+            'ingreso_B': 'V949.2', #M11
+            'listo_para_pesar': 'V949.3', #M12
+            'salida_A': 'V948.4', #M13
+            'salida_B': 'V948.5' #M14
         }
 
+signals_I = {
+            'barrera_A': 'V923.0', #I1
+            'barrera_B': 'V923.1' #I2
+        }
 
-client = snap7.client.Client()
-LOGO_MAC = '' # Buscar IP via ARP 
-LOGO_IP = '192.168.0.4'
+signals_Q = {
+            'semaforo_ingreso_A': 'V942.0', #Q1
+            'semaforo_salida_A': 'V942.1', #Q2
+            'semaforo_ingreso_B': 'V942.2', #Q3
+            'semaforo_salida_B': 'V942.3', #Q4
+        }
+
+PESO_CAMION = 5000 # Para calcular si hay un camion en la balanza
+
+client = snap7.logo.Logo()
+LOGO_MAC = '8C:F3:19:B5:40:16' # Buscar IP via ARP?
+LOGO_IP = '192.168.0.3'
+LOGO_IP = '127.0.0.1'
 RACK = 0
 SLOT = 1
-PORT = 102
+TCP_PORT = 102 # default
 
-client.connect(LOGO_IP, RACK, SLOT)
+client.connect(LOGO_IP, RACK, SLOT, TCP_PORT)
+print('Conexion con logo... OK')
 
-def write_memory(marker, state):
-  memory = client.read_area(Areas.MK, 0, 0, 1)
-  memory = set_bool(memory, 0, marker, state)
-  client.write_area(Areas.MK, 0, 0, memory)
+def write_memory(addr, bit_pos, value):
+    current_byte = client.read(addr)
+
+    if value == 0: # and 
+        bit_mask = 255 - 2**bit_pos
+        current_byte = current_byte & bit_mask
+
+    if value == 1: # or
+        bit_mask = 2**bit_pos
+        current_byte = current_byte | bit_mask
+    
+    client.write(addr, bit_mask)
+
+def write_memory_2(addr_bit, value):
+    addr = addr_bit.split('.')[0]
+    bit_pos = addr_bit.split('.')[1]
+    write_memory(addr, bit_pos, value)
 
 def read_status():
-    return client.read_area(Areas.PA, 0, 0, 1)
+    # Ms V948
+    status = {}
+    for var in signals_M:
+        status[var] = client.read(f'{var}')
+    
+    return status
+    
+def read_status_print():
+    # Is V923
+    for var in signals_I:
+        print(f'Variable {var}:', client.read(f'{var}'))
+    
+    # Qs V942
+    for var in signals_Q:
+        print(f'Variable {var}:', client.read(f'{var}'))
+    
+    # Ms V948
+    for var in signals_M:
+        print(f'Variable {var}:', client.read(f'{var}'))
 
 # Definicion de UI
-app = tk.TK()
-app.title('Control de bascula')
+app = tk.Tk()
+app.title('Control de Bascula')
 
-control_labels = []
+control_labels = {}
 
-def create_app_control(label_name, mem_index)
+def create_app_control(label_name, signal):
     control_label = tk.Label(app, text=label_name, fg='black')
     control_label.pack()
-    control_labels.append(control_label)
+    control_labels[signal] = control_label
 
-    on_button = tk.Button(app, text=f'Activar {label_name}', command=lambda: write_memory(mem_index, True), bg='lime')
+    signal_byte = signals_M[signal].split('.')
+    addr = signal_byte[0]
+    bit_pos= signal_byte[1]
+
+    on_button = tk.Button(app, text=f"Activar {label_name}", command=lambda: write_memory(addr, bit_pos, 1), bg='lime')
     on_button.pack()
 
-    off_button = tk.Button(app, text=f'Desactivar {label_name}', command=lambda: write_memory(mem_index, False), bg='red')
+    off_button = tk.Button(app, text=f"Desactivar {label_name}", command=lambda: write_memory(addr, bit_pos, 0), bg='red')
     off_button.pack()
 
 def read_logo_signals_status():
     while True:
         status = read_status()
+        for key in control_labels.keys():
+            text_color = 'lime'
+            if status[key] == 0:
+                text_color = 'red'
+            control_labels[key].config(fg=text_color)
+        time.sleep(1)
 
-create_app_control('Hab. entrar A', signals_mem['hab_entrar_A'])
-create_app_control('Hab. entrar B', signals_mem['hab_entrar_B'])
-create_app_control('Lista pesada', signals_mem['listo_pesada'])
-create_app_control('Camion a balanza', signals_mem['camion_a_balanza'])
-create_app_control('Balanza en cero', signals_mem['balanza_en_cero'])
-create_app_control('Listo', signals_mem['listo'])
-create_app_control('Listo pesar', signals_mem['listo_pesar'])
+def on_spin_change():
+    valor_pesaje = int(spin_pesaje.get())
+    if valor_pesaje == 0:
+        print('balanza en cero')
+        client.write(signals_M['camion_en_balanza'], 0)
+        client.write(signals_M['balanza_en_cero'], 1)
+    if valor_pesaje > PESO_CAMION:
+        print('cambion en balanza')
+        client.write(signals_M['camion_en_balanza'], 1)
+        client.write(signals_M['balanza_en_cero'], 0)
+
+create_app_control('Hab. general', 'hab_general')
+create_app_control('Hab. entrar A', 'hab_entrar_A')
+create_app_control('Hab. entrar B', 'hab_entrar_B')
+create_app_control('Camion en balanza', 'camion_en_balanza')
+create_app_control('Fin pesada', 'fin_pesada')
+create_app_control('Balanza en cero', 'balanza_en_cero')
+
+spin_pesaje = tk.Spinbox(app, from_=0, to=65000, command=on_spin_change)
+spin_pesaje.pack()
 
 read_signals_thread = threading.Thread(target=read_logo_signals_status, daemon=True)
 read_signals_thread.start()
