848a7665fd184c6be821a08f41a54e9d033ce8e5
Oscar Dorronzoro <oscar.dorronzoro@gmail.com>
2025-02-12 16:06:49 -0300

    compatibility between snap7 library versions

--- s7_server.py
+++ s7_server.py
@@ -2,5 +2,5 @@ import snap7
 
 print('Starting snap7 server...')
 #server = snap7.server.Server()
-server_default = snap7.server.mainloop(tcpport=102, init_standard_values=True)
+server_default = snap7.server.mainloop(102, init_standard_values=True)
 
