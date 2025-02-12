310edd91b697d32c9a991f02f693f9641eea938d
Oscar Dorronzoro <oscar.dorronzoro@gmail.com>
2025-02-12 16:09:04 -0300

    windows installation script, untested

---
+++ windows_install.ps1
@@ -0,0 +1,21 @@
+$arch = (Get-WmiObject Win32_OperatingSystem).OSArchitecture
+$arch = $arch -replace '[\s-]', '' -replace 'bit', '' -replace 'bits', ''
+
+if ($arch -eq "64") {
+    Write-Host "64-bit architecture detected."
+    cp libsnap7/win64/snap7.dll C:/Windows/System32/
+
+} elseif ($arch -eq "32") {
+    Write-Host "32-bit architecture detected."
+    cp libsnap7/win32/snap7.dll C:/Windows/System32/
+
+} else {
+    Write-Host "Unsupported architecture: $arch"
+    exit 1
+}
+
+pip install -r requirements.txt
+#pip install tkinter
+
+exit 0
+
